6b430c1158abd06d09ecf5aea480a70a618f631e
rajratnamaitry <rajratnamaitry@gmail.com>
2022-04-24 22:56:59 +0530

    file updated

--- hash/basic_hash.js
+++ hash/basic_hash.js
@@ -7,3 +7,14 @@ function hash(key, arrayLen) {
   }
   return total;
 }
+// improved has
+function improvedHash(key, arrayLen) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
+}
